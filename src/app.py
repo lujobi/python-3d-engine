b29dcb0d134109c5195418ad19fb5efe94f1df15
Luzian Bieri <luzibier@ethz.ch>
2020-08-12 19:50:02 +0200

    reverted

--- src/app.py
+++ src/app.py
@@ -21,54 +21,15 @@ class App:
     pygame.init()
 
     self.screen = pygame.display.set_mode((width, height))
-
-  def _render_thread(self):
-    render_count = 0
-    fps = None
-    t_last = time.time_ns()
-
-    while self.settings.get('running'):
-
-      if time.time_ns() - t_last > 1E9:
-        t_last = time.time_ns()
-        fps = render_count
-        title = f'{self._title} - at {fps} FPS, {self._requested_tps} TPS'
-        print(title)
-        pygame.display.set_caption(title)
-        render_count = 0     
-
-      self.engine.render(self.screen)
-      self.settings.incr('frame_since_update')
-      render_count += 1
-
+    
   def _stop(self):
     self.settings.set('running', False)
 
-
-  def _tick_thread(self):
-    tick_count = 0
-
-    clock = pygame.time.Clock()
-
-    while self.settings.get('running'):
-
-      if self.engine.tick(self._stop, pygame.event.get(), tick_count):
-        self.settings.set('frame_since_update', 0)
-      
-      tick_count += 1
-      clock.tick(self._requested_tps)
-
-
   def run(self):
     self.settings.set('running', True)
-    render_thread = threading.Thread(target=self._render_thread)
-    tick_thread = threading.Thread(target=self._tick_thread)
-
-    # Start treads and wait for them to stop
-    render_thread.start()
-    tick_thread.start()
-    render_thread.join()
-    tick_thread.join()
+    while self.settings.get('running'):
+      self.engine.render(self.screen)
+      self.engine.tick(self._stop, pygame.event.get(), tick_count)
 
     print('done')
     pygame.quit()
